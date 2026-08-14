36e83b3927b443715410fb1c2c458d4a97717bbc
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-08-14 22:04:48 +0800

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -14,6 +14,8 @@ local Categories = {
     Zone7 = 9071002677,
     Zone8 = 9071002915,
     Zone9 = 9071004505,
+
+    Ring1S = 9070954594,
     -- A category may map to one place id or a list of them. Pit of Misery exists in the
     -- original EToH place AND in "The Eternal Abyss" (15873244701) + its Pit of Misery
     -- subplace (121814103864070) -- the same towers are present in all of them.
