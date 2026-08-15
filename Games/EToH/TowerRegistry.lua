55230211cde49eae94ab75c70317ef477e58034a
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-08-15 15:50:39 +0800

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -158,6 +158,7 @@ return {
         { name = "ToTC",  category = "Ring1S", suggestedTime = { min = "3", sec = "0" } },
         { name = "SoC",  category = "Ring1S", suggestedTime = { min = "2", sec = "0" } },
         { name = "SoHP",  category = "Ring1S", suggestedTime = { min = "2", sec = "0" } },
+        { name = "ToIS",  category = "Ring1S", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToMB",  category = "Ring1S", suggestedTime = { min = "3", sec = "0" } },
         --Pit of Misery
         { name = "ToMDC",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
