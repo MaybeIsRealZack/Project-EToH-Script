81b3cd3f11806e044df4ab5e41e2190aabac8bc2
cslp1 <wrerfsdh5@gmail.com>
2026-08-21 17:43:04 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -281,6 +281,7 @@ return {
         { name = "ToG",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToGF",  category = "Pit-of-Misery", suggestedTime = { min = "0", sec = "5" } },
         { name = "ToVH",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
+        { name = "ToSE",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToTH",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToTRP",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToEV",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
