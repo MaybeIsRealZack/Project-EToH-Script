2404d72cd0baa500a0e1d396471e5e4feca0b6db
cslp1 <wrerfsdh5@gmail.com>
2026-08-26 20:24:43 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -295,11 +295,11 @@ return {
         { name = "ToTRP",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToEV",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToWM",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
+        { name = "ToP",  category = "PitOfMisery", suggestedTime = { min = "7", sec = "5" } },
         { name = "ToBF",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         { name = "ToSF",  category = "Pit-of-Misery", suggestedTime = { min = "5", sec = "5" } },
         --The Eternal Abyss
         { name = "ToMH",  category = "TheEternalAbyss", suggestedTime = { min = "16", sec = "5" } },
-        { name = "ToP",  category = "TheEternalAbyss", suggestedTime = { min = "7", sec = "5" } },
         --TimelessManor
         { name = "ToDC",  category = "TimelessManor", suggestedTime = { min = "5", sec = "5" } },
         --100M Event Replay
